e16c65397ecb4b74b2a2b5b4c33522bd18f1fd1a
ashispavan <ashispavan@live.com>
2020-10-15 15:11:27 +0530

    added first custom hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import useLocalStorage from './hooks/useLocalStorage';
 
 function App() {
+
+  const [value, setvalue] = useLocalStorage('name', "");
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input type="text" value={value} onChange={(event) => setvalue(event.target.value)} />
+  <h2>Name: {value}</h2>
+
       </header>
     </div>
   );
